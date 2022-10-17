e2ce299ab34197dca3f61707dd88bd32aadd6b4f
eunsol-an <dev.eunsol@gmail.com>
2022-10-18 01:59:49 +0900

    Create InputAndOutput1022.java

---
+++ 01_codeup/inputOutput/InputAndOutput1022.java
@@ -0,0 +1,13 @@
+package inputOutput;
+
+import java.util.Scanner;
+
+public class InputAndOutput1022 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String data = sc.nextLine();
+		System.out.println(data);
+	}
+
+}
