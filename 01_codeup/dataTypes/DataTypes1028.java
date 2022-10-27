abc0f830e4eb6a40c27ec2a68979ae7b4976bad0
eunsol-an <dev.eunsol@gmail.com>
2022-10-28 00:02:33 +0900

    Create DataTypes1028.java

---
+++ 01_codeup/dataTypes/DataTypes1028.java
@@ -0,0 +1,14 @@
+package dataTypes;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class DataTypes1028 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		long num = Long.parseLong(br.readLine());
+		System.out.print(num);
+	}
+}
