3fa1036bf519e3e3214eb983619d2427bc64f566
eunsol-an <dev.eunsol@gmail.com>
2022-11-02 00:39:43 +0900

    Create Conversion1036.java

---
+++ 01_codeup/conversion/Conversion1036.java
@@ -0,0 +1,14 @@
+package conversion;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Conversion1036 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = br.readLine().charAt(0);
+		System.out.print(num);
+	}
+}
