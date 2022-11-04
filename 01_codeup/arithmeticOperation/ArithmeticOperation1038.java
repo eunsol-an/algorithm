89c128b70632b82693813d32a373beb8e8e2068d
eunsol-an <dev.eunsol@gmail.com>
2022-11-04 23:33:23 +0900

    Create ArithmeticOperation1038.java

---
+++ 01_codeup/arithmeticOperation/ArithmeticOperation1038.java
@@ -0,0 +1,17 @@
+package arithmeticOperation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class ArithmeticOperation1038 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		long a = Long.parseLong(st.nextToken());
+		long b = Long.parseLong(st.nextToken());
+		System.out.print(a + b);
+	}
+}
