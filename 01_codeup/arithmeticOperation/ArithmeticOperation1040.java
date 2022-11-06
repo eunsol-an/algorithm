127b6656d78171e63bfdbe528d9df3e8ca9f4c0a
eunsol-an <dev.eunsol@gmail.com>
2022-11-06 11:27:27 +0900

    Create ArithmeticOperation1040.java

---
+++ 01_codeup/arithmeticOperation/ArithmeticOperation1040.java
@@ -0,0 +1,14 @@
+package arithmeticOperation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ArithmeticOperation1040 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+		System.out.print(-num);
+	}
+}
