02a7660029e18a1add55ed775dbea9710e28700b
eunsol-an <dev.eunsol@gmail.com>
2022-11-14 19:26:09 +0900

    Create ComparisonOperation1049.java

---
+++ 01_codeup/comparisonOperation/ComparisonOperation1049.java
@@ -0,0 +1,17 @@
+package comparisonOperation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class ComparisonOperation1049 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		System.out.print(a > b ? 1 : 0);
+	}
+}
