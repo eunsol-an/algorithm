d3df36f330e28d9d1463dc5b26efd67cff7dfda5
AN EUNSOL <dev.eunsol@gmail.com>
2022-11-21 09:09:19 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 2.32 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] numbers) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers.length; j++) {
+                if (i != j) {
+                    set.add(numbers[i] + numbers[j]);
+                }
+            }
+        }
+        List<Integer> list = new ArrayList<>(set);
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i).intValue();
+        }
+        Arrays.sort(answer);
+        return answer;
+    }
+}
