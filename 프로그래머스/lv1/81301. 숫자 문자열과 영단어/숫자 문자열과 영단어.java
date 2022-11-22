5f5bf3df3ccec9a15814b059ab32db4005aead97
AN EUNSOL <dev.eunsol@gmail.com>
2022-11-22 11:19:44 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.06 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,10 +1,9 @@
 class Solution {
     public int solution(String s) {
         String[] numberToString = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        String[] StringToNumber = {"0", "1","2", "3", "4", "5", "6", "7", "8", "9"};
         for (int i = 0; i < numberToString.length; i++) {
             if (s.contains(numberToString[i])) {
-                s = s.replaceAll(numberToString[i], StringToNumber[i]);
+                s = s.replaceAll(numberToString[i], String.valueOf(i));
             }
         }
         return Integer.parseInt(s);
