262deaedbed83f721442b630059cc97931095f82
AN EUNSOL <dev.eunsol@gmail.com>
2022-11-23 21:13:28 +0900

    [level 1] Title: 소수 만들기, Time: 0.03 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if ((i != j) && (j != k) && (i != k)) {
+                        if (isPrime(nums[i] + nums[j] + nums[k])) {
+                            answer++;
+                        }
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public boolean isPrime(int n) {
+        int count = 0;
+        for (int j = 2; j <= Math.sqrt(n); j++) {
+            if (n % j == 0) {
+                count++;
+            }
+        }
+        if (count < 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
