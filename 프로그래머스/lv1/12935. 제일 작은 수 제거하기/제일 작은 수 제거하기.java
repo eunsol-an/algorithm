a88f6da7f9f51afc2222c012620a254df12977a6
AN EUNSOL <dev.eunsol@gmail.com>
2022-11-23 20:43:11 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 1.07 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -1,18 +1,16 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 class Solution {
     public int[] solution(int[] arr) {
-        int min = arr[0];
         if (arr.length == 1) {
             return new int[] {-1};
         }
         List<Integer> intList = new ArrayList<>();
         for (int i = 0; i < arr.length; i++) {
             intList.add(arr[i]);
-            if (min > arr[i]) {
-                min = arr[i];
-            }
         }
+        Integer min = Collections.min(intList);
         for (int i = 0; i < intList.size(); i++) {
             if (intList.get(i) == min)  {
                 intList.remove(i);
