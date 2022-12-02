5da57330a8398ac58af90802d82991399fef4aea
eunsol-an <dev.eunsol@gmail.com>
2022-12-02 23:15:46 +0900

    Create Condition1065.java

---
+++ 01_codeup/condition/Condition1065.java
@@ -0,0 +1,29 @@
+package condition;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Condition1065 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		
+		if(a % 2 == 0) {
+			System.out.println(a);
+		}
+		if(b % 2 == 0) {
+			System.out.println(b);
+		}
+		if(c % 2 == 0) {
+			System.out.println(c);
+		}
+
+	}
+
+}
