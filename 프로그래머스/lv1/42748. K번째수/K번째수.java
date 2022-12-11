c9e8534714bebc2d1d9d9a95887d7351e4332fca
AN EUNSOL <dev.eunsol@gmail.com>
2022-12-11 16:27:21 +0900

    [level 1] Title: K번째수, Time: 0.44 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.java
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -1,9 +1,7 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        List<Integer> answerList = new ArrayList<>();
+        int[] answer = new int[commands.length];
         for (int i = 0; i < commands.length; i++) {
             int a = commands[i][0];
             int b = commands[i][1];
@@ -11,11 +9,7 @@ class Solution {
 
             int[] result = Arrays.copyOfRange(array, a - 1, b);
             Arrays.sort(result);
-            answerList.add(result[c - 1]);
-        }
-        int[] answer = new int[answerList.size()];
-        for (int i = 0; i < answerList.size(); i++) {
-            answer[i] = answerList.get(i).intValue();
+            answer[i] = result[c - 1];
         }
         return answer;
     }
--- 프로그래머스/lv1/42748. K번째수/README.md
+++ 프로그래머스/lv1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=java#) 
 
 ### 성능 요약
 
-메모리: 73 MB, 시간: 0.47 ms
+메모리: 71.6 MB, 시간: 0.44 ms
 
 ### 구분
 
