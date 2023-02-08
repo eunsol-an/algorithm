03557ed137f65424a9759911508998db2ba847be
AN EUNSOL <dev.eunsol@gmail.com>
2023-02-08 21:36:29 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 65.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
@@ -1,10 +1,5 @@
 class Solution {
     public int solution(int n, int k) {
-        int answer = (n * 12000) + (k * 2000);
-        if (n / 10 >= 1) {
-            int service = n / 10;
-            answer = answer - (service * 2000);
-        }
-        return answer;
+        return (n * 12000) + (k * 2000) - (n / 10 * 2000);
     }
 }
