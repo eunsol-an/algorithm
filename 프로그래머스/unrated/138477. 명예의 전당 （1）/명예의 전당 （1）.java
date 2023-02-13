f1b4b3a0b1ef5b25598887a911b1a96f83e80c5b
AN EUNSOL <dev.eunsol@gmail.com>
2023-02-14 00:30:14 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.43 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -1,16 +1,15 @@
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.PriorityQueue;
 
 class Solution {
     public int[] solution(int k, int[] score) {
         int[] result = new int[score.length];
-		ArrayList<Integer> rank = new ArrayList<>();
+        PriorityQueue<Integer> rank = new PriorityQueue<>();
 
-		for (int i = 0; i < score.length; i++) {
-			rank.add(score[i]);
-			if (i >= k) rank.remove(Collections.min(rank));
-			result[i] = Collections.min(rank);
-		}
-		return result;
+        for (int i = 0; i < score.length; i++) {
+            rank.add(score[i]);
+            if (i >= k) rank.poll();
+            result[i] = rank.peek();
+        }
+        return result;
     }
 }
