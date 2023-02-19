695de7a0b1381b7b2829d06e1c727fdb365d103e
AN EUNSOL <dev.eunsol@gmail.com>
2023-02-19 23:56:19 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+        int odd = 0;
+        int even = 0;
+        for (int i : num_list) {
+            if (i % 2 == 0) even++;
+            else odd++;
+        }
+        answer[0] = even;
+        answer[1] = odd;
+        return answer;
+    }
+}
