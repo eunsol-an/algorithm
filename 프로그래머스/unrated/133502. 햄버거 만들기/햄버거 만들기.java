0db989b9ba62a9a3cc361fdef34b29d280aa8a2f
AN EUNSOL <dev.eunsol@gmail.com>
2023-02-20 09:41:54 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.java
@@ -2,20 +2,16 @@ import java.util.Stack;
 class Solution {
     public int solution(int[] ingredient) {
         int answer = 0;
-        Stack<Integer> stack = new Stack<>();
+        int index = 0;
+        int[] stack = new int[ingredient.length];
         for (int i = 0; i < ingredient.length; i++) {
-            stack.push(ingredient[i]);
-            if (stack.size() >= 4) {
-                if (stack.get(stack.size() - 4) == 1
-                        && stack.get(stack.size() - 3) == 2
-                        && stack.get(stack.size() - 2) == 3
-                        && stack.get(stack.size() - 1) == 1) {
-                    answer++;
-                    stack.pop();
-                    stack.pop();
-                    stack.pop();
-                    stack.pop();
-                }
+            stack[index++] = ingredient[i];
+            if (index >= 4 && stack[index - 1] == 1
+            && stack[index - 2] == 3
+            && stack[index - 3] == 2
+            && stack[index - 4] == 1) {
+                answer++;
+                index -= 4;
             }
         }
         return answer;
