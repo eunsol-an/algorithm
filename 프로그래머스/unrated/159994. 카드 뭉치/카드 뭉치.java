2dbe384e7ccc857dc273dcd274ff2affa0019d12
AN EUNSOL <dev.eunsol@gmail.com>
2023-03-01 11:35:10 +0900

    [unrated] Title: 카드 뭉치, Time: 0.10 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        List<String> goalList = new ArrayList<>(Arrays.asList(goal));
+        List<String> cards1List = new ArrayList<>(Arrays.asList(cards1));
+        List<String> cards2List = new ArrayList<>(Arrays.asList(cards2));
+
+        for (String s : goalList) {
+            if (!cards1List.isEmpty() && cards1List.get(0).equals(s)) {
+                cards1List.remove(0);
+            } else {
+                if (!cards2List.isEmpty() && cards2List.get(0).equals(s)) {
+                    cards2List.remove(0);
+                } else {
+                    return "No";
+                }
+
+            }
+        }
+        return "Yes";
+    }
+}
