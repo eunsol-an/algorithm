f95c130ec32f4ca9c9f20b117ecbe68566670ac7
AN EUNSOL <dev.eunsol@gmail.com>
2023-03-01 23:03:31 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.java
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.java
@@ -1,19 +1,15 @@
 class Solution {
     public int solution(int[] number) {
         int answer = 0;
-        int start = 0;
-		while(start <= number.length-3 ) {
-		
-		for(int i = start+1; i< number.length; i++) {
-			for( int j = i+1; j< number.length; j++) {
-				if(number[start] + number[i] + number[j] == 0) {
-					answer++;
-				}
-			}
-		}
-		
-		start ++;
-		}
+        for (int i = 0; i < number.length; i++) {
+            for (int j = i + 1; j < number.length; j++) {
+                for (int k = j + 1; k < number.length; k++) {
+                    if (number[i] + number[j] + number[k] == 0) {
+                        answer++;
+                    }
+                }
+            }
+        }
         return answer;
     }
 }
