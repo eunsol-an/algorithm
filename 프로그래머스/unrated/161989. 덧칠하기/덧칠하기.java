e7693236c112954feceefe30ab3dfa9631ca15c4
AN EUNSOL <dev.eunsol@gmail.com>
2023-03-30 09:42:50 +0900

    [unrated] Title: 덧칠하기, Time: 0.82 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
@@ -1,11 +1,12 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
         int answer = 0;
-        int max = 0;
-        for (int one : section) {
-            if (one < max) continue;
-            answer += 1;
-            max = one + m;
+        int roller = section[0];
+        for (int point : section) {
+            if (point >= roller) {
+                answer += 1;
+                roller = point + m;
+            }
         }
         return answer;
     }
