84a739b642d86f7a74cb8267b69c2f414369ca1a
AN EUNSOL <dev.eunsol@gmail.com>
2023-03-30 20:45:54 +0900

    [unrated] Title: 귤 고르기, Time: 53.96 ms, Memory: 100 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        int cnt = 0;
+        List<Integer> countList = new ArrayList<>();
+
+        Arrays.sort(tangerine);
+        for (int i = 0; i < tangerine.length; i++) {
+            cnt++;
+            if ((i < tangerine.length - 1 && tangerine[i] != tangerine[i + 1])
+                    || (i == tangerine.length - 1 && cnt > 0)) {
+                countList.add(cnt);
+                cnt = 0;
+            }
+        }
+        
+        countList.sort(Collections.reverseOrder());
+        int i = 0;
+        while (k > 0) {
+            k -= countList.get(i);
+            i++;
+            answer++;
+        }
+        
+        return answer;
+    }
+}
