2b21ff0fafa0b19b4ab7dc3b610349a380f7efff
AN EUNSOL <dev.eunsol@gmail.com>
2023-05-14 23:47:48 +0900

    [unrated] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,12 @@
+-- 코드를 입력하세요
+SELECT CONCAT('/home/grep/src/'
+              , F.BOARD_ID
+              , '/'
+              , FILE_ID
+              , FILE_NAME
+              , FILE_EXT) AS FILE_PATH
+FROM USED_GOODS_FILE F
+LEFT JOIN USED_GOODS_BOARD B
+ON F.BOARD_ID = B.BOARD_ID
+WHERE VIEWS = (SELECT MAX(VIEWS) FROM USED_GOODS_BOARD)
+ORDER BY FILE_ID DESC
