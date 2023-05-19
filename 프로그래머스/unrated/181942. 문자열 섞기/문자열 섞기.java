bf5eda8150434465286cd6a07974d42ad3e72b70
AN EUNSOL <dev.eunsol@gmail.com>
2023-05-19 22:30:02 +0900

    [unrated] Title: 문자열 섞기, Time: 0.06 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.java
@@ -1,10 +1,9 @@
 class Solution {
     public String solution(String str1, String str2) {
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
         for(int i = 0; i < str1.length(); i++) {
-            answer += str1.charAt(i);
-            answer += str2.charAt(i);
+            answer.append(str1.charAt(i)).append(str2.charAt(i));
         }
-        return answer;
+        return answer.toString();
     }
 }
