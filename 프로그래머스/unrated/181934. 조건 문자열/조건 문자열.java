f0570b1ac7f8d00f7fe02bbb232873ccec99e9d0
AN EUNSOL <dev.eunsol@gmail.com>
2023-05-21 23:48:48 +0900

    [unrated] Title: 조건 문자열, Time: 0.04 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(String ineq, String eq, int n, int m) {
+        if (ineq.equals(">")) {
+            if (eq.equals("=")) {
+                return n >= m ? 1 : 0;
+            }
+            else {
+                return n > m ? 1 : 0;
+            }
+        } else {
+            if (eq.equals("=")) {
+                return n <= m ? 1 : 0;
+            }
+            else {
+                return n < m ? 1 : 0;
+            }
+        }
+    }
+}
