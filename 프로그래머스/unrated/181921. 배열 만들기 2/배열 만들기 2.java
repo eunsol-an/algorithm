761c7c114efe54a1dd09adc143d577e1ee272da7
AN EUNSOL <dev.eunsol@gmail.com>
2023-05-31 23:54:26 +0900

    [unrated] Title: 배열 만들기 2, Time: 17.29 ms, Memory: 88.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int l, int r) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = l; i <= r; i++) {
+            int count = 0;
+            String str = Integer.toString(i);
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(j) == 48 || str.charAt(j) == 53) {
+                    count ++;
+                }
+            }
+            if(count == str.length()) {
+                list.add(i);
+            }
+        }
+        int[] answer = list.stream().mapToInt(i->i).toArray();
+        int[] empty = {-1};
+        if(answer.length == 0) return empty;
+        return answer;
+    }
+}
