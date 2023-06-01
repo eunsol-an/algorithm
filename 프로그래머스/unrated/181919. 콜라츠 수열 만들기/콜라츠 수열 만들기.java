96e3216afce94f8bea34bfa3265f3138d021be8d
AN EUNSOL <dev.eunsol@gmail.com>
2023-06-01 23:32:05 +0900

    [unrated] Title: 콜라츠 수열 만들기, Time: 0.03 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public List<Integer> solution(int n) {
+        int[] answer = {};
+        List<Integer> collatz = new ArrayList<>();
+        collatz.add(n);
+        while (n > 1) {
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else {
+                n = (3 * n + 1);
+            }
+            collatz.add(n);
+        }
+        return collatz;
+    }
+}
