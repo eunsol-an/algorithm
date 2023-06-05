6c5f2dd8c0024ebc045f6e0b879c9758968cee33
AN EUNSOL <dev.eunsol@gmail.com>
2023-06-05 23:30:51 +0900

    [unrated] Title: 9로 나눈 나머지, Time: 0.02 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.java
+++ 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -2,8 +2,9 @@ class Solution {
     public int solution(String number) {
         int answer = 0;
         for (int i = 0; i < number.length(); i++) {
-            answer += Integer.parseInt(String.valueOf(number.charAt(i)));
+            answer += number.charAt(i) - '0';
         }
-        return answer % 9;
+        int result = answer % 9;
+        return result;
     }
 }
