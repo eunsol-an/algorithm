0d8cfe26cad84565c73e6a06e9331576149104b1
AN EUNSOL <dev.eunsol@gmail.com>
2023-06-10 23:45:22 +0900

    [unrated] Title: 배열 만들기 5, Time: 4.39 ms, Memory: 96.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.java
+++ 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.java
@@ -1,13 +1,20 @@
 import java.util.*;
 class Solution {
-    public List<Integer> solution(String[] intStrs, int k, int s, int l) {
-        List<Integer> answer = new ArrayList<>();
+    public int[] solution(String[] intStrs, int k, int s, int l) {
+        List<Integer> list = new ArrayList<>();
+
         for (int i = 0; i < intStrs.length; i++) {
-            int num = Integer.parseInt(intStrs[i].substring(s, s + l));
-            if (num > k) {
-                answer.add(num);
+            int tmp = Integer.parseInt(intStrs[i].substring(s, s + l));
+            if (tmp > k) {
+                list.add(tmp);
             }
         }
+        int[] answer = new int[list.size()];
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i);
+        }
+
         return answer;
     }
 }
