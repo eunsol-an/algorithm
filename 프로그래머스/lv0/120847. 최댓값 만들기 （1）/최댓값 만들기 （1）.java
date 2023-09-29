62896e9ca425215ef63e522548c093f339361ad6
AN EUNSOL <dev.eunsol@gmail.com>
2023-09-29 23:03:46 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.02 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int max = 0;
+
+        for(int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] * numbers[j] > max) {
+                    max = numbers[i] * numbers[j];
+                }
+            }
+        }
+        return max;
+    }
+}
