0849518bed85522003a686352f06b46de0f3ffde
AN EUNSOL <dev.eunsol@gmail.com>
2023-10-08 19:37:19 +0900

    [level 2] Title: N-Queen, Time: 166.30 ms, Memory: 94.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12952. N－Queen/N－Queen.java
@@ -0,0 +1,45 @@
+class Solution {
+    
+    static int[] arr;
+    static int cnt;
+    
+    public int solution(int n) {
+        int answer = 0;
+        arr = new int[n];
+
+        bt(n ,0);
+
+        answer = cnt;
+        return answer;
+    }
+    
+    static void bt(int n, int row) {
+        
+        if(n == row) {
+            cnt++;
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr[row] = i;
+            if(isPossible(row)) {
+                bt(n ,row + 1);
+            }
+        }
+    }
+
+    
+    static boolean isPossible(int row) {
+        for (int i = 0; i < row; i++) {
+            
+            if(arr[i] == arr[row]) {
+                return false;
+            }
+
+            if(Math.abs(row - i) == Math.abs(arr[row] - arr[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
