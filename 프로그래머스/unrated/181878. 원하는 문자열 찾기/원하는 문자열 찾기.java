be26cc8f10e054284669277dc329b1c07b2f5415
AN EUNSOL <dev.eunsol@gmail.com>
2023-12-05 23:57:56 +0900

    [level unrated] Title: 원하는 문자열 찾기, Time: 6.67 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        if(myString.toLowerCase().contains(pat.toLowerCase())){
+            return 1;
+        }
+        return 0;
+    }
+}
