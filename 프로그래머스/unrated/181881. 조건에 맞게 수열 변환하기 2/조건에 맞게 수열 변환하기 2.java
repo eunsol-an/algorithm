16649c395cc001d82ea585b0f1f3ce2d18fc4c04
AN EUNSOL <dev.eunsol@gmail.com>
2024-01-03 18:33:21 +0900

    [level unrated] Title: 조건에 맞게 수열 변환하기 2, Time: 0.04 ms, Memory: 80.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int[] arr) {
+        int answer = 0;
+        int count = 0;
+        
+        int[] beforeArr = new int[arr.length];
+        while (count != arr.length) {
+            count = 0;
+            beforeArr = arr.clone();
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] % 2 == 0 && arr[i] >= 50) {
+                    arr[i] /= 2;
+                } else if (arr[i] % 2 != 0 && arr[i] < 50) {
+                    arr[i] = arr[i] * 2 + 1;
+                }
+                
+                if (arr[i] == beforeArr[i]) count++;
+            }
+            answer++;
+        }
+        
+        return answer - 1;
+    }
+}
