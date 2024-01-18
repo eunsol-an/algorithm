024779d01b96593fce2f10102c584c0c15e59f9d
AN EUNSOL <dev.eunsol@gmail.com>
2024-01-19 01:56:59 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.36 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] sides) {
+        Arrays.sort(sides);
+        if (sides[2] < (sides[0] + sides[1])) return 1;
+        return 2;
+    }
+}
