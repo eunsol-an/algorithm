8a178554d2d73baa826c9b81c3d9bd848f927024
AN EUNSOL <dev.eunsol@gmail.com>
2024-03-04 23:45:26 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.05 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[][] dots) {
+        int w = 0;
+        int h = 0;
+        int x = dots[0][0];
+        int y = dots[0][1];
+        for (int i = 1; i < dots.length; i++) {
+            if (x != dots[i][0]) w = Math.abs(x - dots[i][0]);
+            if (y != dots[i][1]) h = Math.abs(y - dots[i][1]);
+        }
+        return w * h;
+    }
+}
