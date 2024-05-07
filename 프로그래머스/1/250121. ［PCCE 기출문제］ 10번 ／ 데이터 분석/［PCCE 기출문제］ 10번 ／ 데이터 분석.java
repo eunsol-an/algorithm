9a13ec649f96e82f4765d7c5931c1d99a98d49b1
AN EUNSOL <dev.eunsol@gmail.com>
2024-05-07 23:35:22 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 4.79 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -0,0 +1,18 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Arrays;
+
+class Solution {
+    public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
+        Map<String, Integer> colOrder = new HashMap<>();
+        colOrder.put("code", 0);
+        colOrder.put("date", 1);
+        colOrder.put("maximum", 2);
+        colOrder.put("remain", 3);
+        
+        int[][] filteredData = Arrays.stream(data).filter(x -> x[colOrder.get(ext)] < val_ext).toArray(int[][]::new);
+        Arrays.sort(filteredData, (o1, o2) ->  o1[colOrder.get(sort_by)] - o2[colOrder.get(sort_by)]);
+
+        return filteredData;
+    }
+}
