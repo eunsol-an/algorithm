954db7f90261eddbf55f622777f94cdc18c99222
AN EUNSOL <dev.eunsol@gmail.com>
2024-06-17 23:52:11 +0900

    [Bronze V] Title: 엄청난 부자2, Time: 216 ms, Memory: 17872 KB -BaekjoonHub

---
+++ 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.java
@@ -0,0 +1,16 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        BigInteger a = scanner.nextBigInteger();
+        BigInteger b = scanner.nextBigInteger();
+
+        System.out.println(a.divide(b));
+        System.out.println(a.remainder(b));
+
+
+    }
+}
