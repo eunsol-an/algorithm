4bf5f10c4745ec042a521faf904e55a940445401
AN EUNSOL <dev.eunsol@gmail.com>
2024-06-30 00:45:45 +0900

    [Bronze V] Title: 콜센터, Time: 96 ms, Memory: 13856 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("     /~\\\n" +
+                "    ( oo|\n" +
+                "    _\\=/_\n" +
+                "   /  _  \\\n" +
+                "  //|/.\\|\\\\\n" +
+                " ||  \\ /  ||\n" +
+                "============\n" +
+                "|          |\n" +
+                "|          |\n" +
+                "|          |");
+    }
+}
