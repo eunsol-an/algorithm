45cf059e17e9406c4af199550a4d1cc1a914e4c7
AN EUNSOL <dev.eunsol@gmail.com>
2024-07-07 23:55:08 +0900

    [Bronze V] Title: 조별과제를 하려는데 조장이 사라졌다, Time: 208 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n % 5 == 0) {
+            System.out.println(n / 5);
+        } else {
+            System.out.println(n / 5 + 1);
+        }
+    }
+}
