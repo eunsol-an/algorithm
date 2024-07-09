944f52b8393e474b33d5963f6ca27c09abf6f545
AN EUNSOL <dev.eunsol@gmail.com>
2024-07-09 23:29:37 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 104 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
+public class Main {
+ 
+	public static void main(String[] args) throws IOException {
+		     
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] str = br.readLine().split(" ");
+		int A = Integer.parseInt(str[0]);
+		int B = Integer.parseInt(str[1]);
+		
+		System.out.println((A > B) ? ">" : ((A < B) ? "<" : "==" ));
+ 
+	}
+}
