9b357de25b5903d47a37e4e8343e3f1698aab549
AN EUNSOL <dev.eunsol@gmail.com>
2024-07-10 23:27:54 +0900

    [Bronze V] Title: 검증수, Time: 168 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            int temp = scanner.nextInt();
+            sum += temp * temp;
+        }
+
+        int result = sum % 10;
+        System.out.println(result);
+    }
+}
