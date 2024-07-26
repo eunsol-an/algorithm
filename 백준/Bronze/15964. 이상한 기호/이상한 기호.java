0c7c9ed587710c8dad359f849c06a89a5b2a05ad
AN EUNSOL <dev.eunsol@gmail.com>
2024-07-26 23:41:20 +0900

    [Bronze V] Title: 이상한 기호, Time: 128 ms, Memory: 14192 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        System.out.print((a + b) * (a - b));
+    }
+}
