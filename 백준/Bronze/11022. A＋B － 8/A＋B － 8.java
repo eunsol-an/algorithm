2294dbb12b71e5fad4e192a1f6cc89e5ca295324
AN EUNSOL <dev.eunsol@gmail.com>
2024-08-15 23:32:47 +0900

    [Bronze V] Title: A+B - 8, Time: 136 ms, Memory: 16396 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,25 @@
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < n; i++) {
+			String str = br.readLine();
+			int a = Integer.parseInt(str.split(" ")[0]);
+			int b = Integer.parseInt(str.split(" ")[1]);
+			
+			bw.write("Case #" + (i + 1) + ": " + a + " + " + b + " = " + (a + b) + "\n");
+		}
+		br.close();
+		bw.flush();
+		bw.close();		
+	}
+}
