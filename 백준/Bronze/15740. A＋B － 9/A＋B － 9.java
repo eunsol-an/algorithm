083f78f742817820060008f83f33149ac34c93a1
AN EUNSOL <dev.eunsol@gmail.com>
2024-08-18 23:39:48 +0900

    [Bronze V] Title: A+B - 9, Time: 204 ms, Memory: 17340 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15740. A＋B － 9/A＋B － 9.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BigInteger a = new BigInteger(st.nextToken());
+        BigInteger b = new BigInteger(st.nextToken());
+        System.out.print(a.add(b));
+    }
+}
