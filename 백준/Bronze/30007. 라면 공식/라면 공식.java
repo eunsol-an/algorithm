43392dc943210454b2c8f0a1f96df9282688d16d
AN EUNSOL <dev.eunsol@gmail.com>
2024-08-25 23:55:38 +0900

    [Bronze V] Title: 라면 공식, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/30007. 라면 공식/라면 공식.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+
+            int w = a * (x - 1) + b;
+
+            System.out.println(w);
+        }
+    }
+}
