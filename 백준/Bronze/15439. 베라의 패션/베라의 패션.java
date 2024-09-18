53ce25fca1dce2f5a0333039b9391ae18896fb03
AN EUNSOL <dev.eunsol@gmail.com>
2024-09-18 23:58:26 +0900

    [Bronze IV] Title: 베라의 패션, Time: 120 ms, Memory: 15832 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        br.close();
+
+        bw.write(N * (N - 1) + "\n");
+
+        bw.flush();
+        bw.close();
+    }
+
+}
