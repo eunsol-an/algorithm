878e82e94281dfc5156b04004e82b466f8c6bc48
AN EUNSOL <dev.eunsol@gmail.com>
2024-10-04 22:29:30 +0900

    [Bronze IV] Title: 입실 관리, Time: 100 ms, Memory: 14340 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5524. 입실 관리/입실 관리.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        
+        while (n --> 0) {
+            String cur = br.readLine();
+            sb.append(cur.toLowerCase()).append('\n');
+        }
+        
+        System.out.print(sb);
+    }
+}
