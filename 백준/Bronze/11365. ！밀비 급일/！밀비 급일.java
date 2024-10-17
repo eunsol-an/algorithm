eb169ae2a84e96096582b2c13df4527104cd48ba
AN EUNSOL <dev.eunsol@gmail.com>
2024-10-17 23:51:10 +0900

    [Bronze IV] Title: !밀비 급일, Time: 100 ms, Memory: 14344 KB -BaekjoonHub

---
+++ 백준/Bronze/11365. ！밀비 급일/！밀비 급일.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        while (true) {
+            String s = br.readLine();
+            if (s.equals("END")) break;
+            
+            StringBuilder reverse = new StringBuilder(s);
+            reverse = reverse.reverse();
+            sb.append(reverse + "\n");
+        }
+        
+        System.out.print(sb);
+    }
+}
