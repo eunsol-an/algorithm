7d8fc4e429232eb85b9296967252b3fd43d978f7
AN EUNSOL <dev.eunsol@gmail.com>
2024-11-06 22:32:35 +0900

    [Bronze IV] Title: 나는 행복합니다~, Time: 124 ms, Memory: 15960 KB -BaekjoonHub

---
+++ 백준/Bronze/14652. 나는 행복합니다～/나는 행복합니다～.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        bw.write(String.valueOf((K / M) + " " + (K - (M * (K / M)))));
+        bw.flush();
+        bw.close();
+
+    }
+}
