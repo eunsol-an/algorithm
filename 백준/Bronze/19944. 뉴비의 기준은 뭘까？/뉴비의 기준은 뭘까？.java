7cabe420255d13cf4b60dc7868ac15b5437523c0
AN EUNSOL <dev.eunsol@gmail.com>
2024-11-11 23:41:27 +0900

    [Bronze IV] Title: 뉴비의 기준은 뭘까?, Time: 176 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Bronze/19944. 뉴비의 기준은 뭘까？/뉴비의 기준은 뭘까？.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(), M = sc.nextInt();
+
+        if (M < 3) {
+            System.out.println("NEWBIE!");
+        } else if (M <= N) {
+            System.out.println("OLDBIE!");
+        } else {
+            System.out.println("TLE!");
+        }
+    }
+}
