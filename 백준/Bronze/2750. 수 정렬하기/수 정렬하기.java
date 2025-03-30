4f969ed601ab715a2fe193a1ef2c3602c7418944
AN EUNSOL <dev.eunsol@gmail.com>
2025-03-30 21:50:57 +0900

    [Bronze II] Title: 수 정렬하기, Time: 160 ms, Memory: 15032 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = 0; j < N - 1 - i; j++) {
+                if (A[j] > A[j + 1]) {
+                    int temp = A[j];
+                    A[j] = A[j + 1];
+                    A[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(A[i]);
+        }
+    }
+}
