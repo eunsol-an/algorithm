bc1e96278be82a77793effb1691a5672ee979c78
AN EUNSOL <dev.eunsol@gmail.com>
2025-06-10 22:51:35 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 920 ms, Memory: 112784 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -2,7 +2,7 @@ import java.io.*;
 
 public class Main {
     public static int[] A, tmp;
-
+    public static long result;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -10,52 +10,60 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         A = new int[N + 1];
         tmp = new int[N + 1];
-        
         for (int i = 1; i <= N; i++) {
             A[i] = Integer.parseInt(br.readLine());
         }
+
         mergeSort(1, N);
-        
+
         for (int i = 1; i <= N; i++) {
             bw.write(A[i] + "\n");
         }
-        
         bw.flush();
         bw.close();
     }
 
-    private static void mergeSort(int s, int e) {
-        if (e - s < 1) return;
-        int m = s + (e - s) / 2;
-        mergeSort(s, m);
-        mergeSort(m + 1, e);
-        for (int i = s; i <= e; i++) {
+    private static void mergeSort(int start, int end) {
+        if (end - start < 1) return;
+
+        int m = start + (end - start) / 2;
+
+        mergeSort(start, m);
+
+        mergeSort(m + 1, end);
+
+        for (int i = start; i <= end; i++) {
             tmp[i] = A[i];
         }
-        int k = s;
-        int index1 = s;
-        int index2 = m + 1;
-        while (index1 <= m && index2 <= e) {
-            if (tmp[index1] > tmp[index2]) {
-                A[k] = tmp[index2];
-                k++;
-                index2++;
+
+        int index = start;
+        int pointer1 = start;
+        int pointer2 = m + 1;
+
+        while (pointer1 <= m && pointer2 <= end) {
+            if (tmp[pointer1] < tmp[pointer2])  {
+                A[index] = tmp[pointer1];
+                index++;
+                pointer1++;
             } else {
-                A[k] = tmp[index1];
-                k++;
-                index1++;
+                A[index] = tmp[pointer2];
+                index++;
+                pointer2++;
             }
         }
-        while (index1 <= m) {
-            A[k] = tmp[index1];
-            k++;
-            index1++;
+
+        while (pointer1 <= m) {
+            A[index] = tmp[pointer1];
+            index++;
+            pointer1++;
         }
-        while (index2 <= e) {
-            A[k] = tmp[index2];
-            k++;
-            index2++;
+
+        while (pointer2 <= end) {
+            A[index] = tmp[pointer2];
+            index++;
+            pointer2++;
         }
+
 
     }
 }
